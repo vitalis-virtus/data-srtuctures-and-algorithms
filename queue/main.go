14bf06c704298bb37391fcacd0000b72d4b7d02d
vitalis-virtus <vshayuk@gmail.com>
2023-05-30 14:20:02 +0300

    implemented queue

--- queue/main.go
+++ queue/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+type Queue struct {
+	items []int
+}
+
+func (q *Queue) Enqueue(i int) {
+	q.items = append(q.items, i)
+}
+
+func (q *Queue) Dequeue() int {
+	removed := q.items[0]
+	q.items = q.items[1:]
+	return removed
+}
+
+func main() {
+	myQueue := Queue{}
+	myQueue.Enqueue(100)
+	myQueue.Enqueue(200)
+	myQueue.Enqueue(300)
+	fmt.Println(myQueue)
+	fmt.Println(myQueue.Dequeue())
+	fmt.Println(myQueue)
 }
