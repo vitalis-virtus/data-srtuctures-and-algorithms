2f22d45cc0f1113c24a4f7666b2f0f53e1836e6b
vitalis-virtus <vshayuk@gmail.com>
2023-05-30 14:41:52 +0300

    implemented linked-list

--- linked-list/main.go
+++ linked-list/main.go
@@ -19,6 +19,38 @@ func (l *linkedList) prepend(n *node) {
 	l.length++
 }
 
+func (l linkedList) printListData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next
+		l.length--
+	}
+	fmt.Print("\n")
+}
+
+func (l *linkedList) deleteWithValue(value int) {
+	if l.length == 0 {
+		return
+	}
+
+	if l.head.data == value {
+		l.head = l.head.next
+		l.length--
+		return
+	}
+
+	previousToDelete := l.head
+	for previousToDelete.next.data != value {
+		if previousToDelete.next.next == nil {
+			return
+		}
+		previousToDelete = previousToDelete.next
+	}
+	previousToDelete.next = previousToDelete.next.next
+	l.length--
+}
+
 func main() {
 	myList := linkedList{}
 	node1 := &node{data: 45}
@@ -28,5 +60,11 @@ func main() {
 	myList.prepend(node1)
 	myList.prepend(node2)
 	myList.prepend(node3)
-	fmt.Println(myList)
+	myList.printListData()
+	myList.deleteWithValue(90)
+	myList.deleteWithValue(1)
+	emptyList := linkedList{}
+	emptyList.deleteWithValue(100)
+	myList.printListData()
+
 }
